2351f78e7e316be81cd7f59675e3ed3b2f2bb8cb
Roberto Dominguez <7903106@alumnos.ufv.es>
2023-03-30 20:16:12 +0200

    Metodos get y post de Pago y Direccion funcionando y almacenandose en json

--- src/main/java/com/ufv/userAPI/Http_Controller.java
+++ src/main/java/com/ufv/userAPI/Http_Controller.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 public class Http_Controller {
     JSON_Methods jm= new JSON_Methods();
 
+    //------------------------------------------------------GET---------------------------------------------------------
     @GetMapping(value = "/Clientes")
     public ArrayList<Cliente> mostrar() throws NoSuchMethodException {
         return jm.get("clientes.json", Cliente.class);
@@ -23,6 +24,18 @@ public class Http_Controller {
     public ArrayList<Empleado> mostrar_Empleado() throws NoSuchMethodException {
         return jm.get("empleados.json", Empleado.class);
     }
+
+    @GetMapping(value = "/Fichajes")
+    public ArrayList<Fichaje> mostrar_Fichajes() throws NoSuchMethodException {
+        return jm.get("fichajes.json", Fichaje.class);
+    }
+
+    @GetMapping(value = "/Pago")
+    public ArrayList<MetodoPagoCliente> mostrar_Pago() throws NoSuchMethodException {
+        return jm.get("pago.json", MetodoPagoCliente.class);
+    }
+
+    //------------------------------------------------------POST--------------------------------------------------------
 
     @PostMapping(value = "/Clientes", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Cliente> agregar_Cliente(@RequestBody Cliente cliente) throws IOException, NoSuchMethodException {
@@ -34,6 +47,18 @@ public class Http_Controller {
     public ResponseEntity<Empleado> agregar_Empleado(@RequestBody Empleado empleado) throws IOException, NoSuchMethodException {
         jm.post("empleados.json", Empleado.class, empleado);
         return new ResponseEntity<>(empleado, HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping(value = "/Fichajes", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Fichaje> agregar_Fichajes(@RequestBody Fichaje fichaje) throws IOException, NoSuchMethodException {
+        jm.post("fichajes.json", Fichaje.class, fichaje);
+        return new ResponseEntity<>(fichaje, HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping(value = "/Pago", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MetodoPagoCliente> agregar_Pago(@RequestBody MetodoPagoCliente pago) throws IOException, NoSuchMethodException {
+        jm.post("pago.json", MetodoPagoCliente.class, pago);
+        return new ResponseEntity<>(pago, HttpStatus.ACCEPTED);
     }
 
 
